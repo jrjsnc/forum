6cb5f492e6bbd53b49a5d6483e9026e043658733
Miro Sotak <you@example.com>
2018-01-22 15:12:58 +0100

    vytvoreny balik entity plus UserServiceJPA

--- src/main/java/forum/entity/User.java
+++ src/main/java/forum/entity/User.java
@@ -1,5 +1,18 @@
 package forum.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+	@Id
+	@GeneratedValue
+	private int ident;
+	private String login;
+	private String password;
+	private String email;
+	
+	
 
 }
