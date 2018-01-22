44f142746f621f776cbf9ff89dfe47f74a3012ca
cekan <denisacekanova.de@gmail.com>
2018-01-22 15:16:37 +0100

    entita Topic

--- src/main/java/forum/entity/Topic.java
+++ src/main/java/forum/entity/Topic.java
@@ -1,5 +1,27 @@
 package forum.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Topic {
+	
+	@Id
+	@GeneratedValue
+	private int ident;
+	private String name;
+	public int getIdent() {
+		return ident;
+	}
+	public void setIdent(int ident) {
+		this.ident = ident;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
