0e74b3a3cd8d85dece383c886e9f1caec4229110
Miro Sotak <you@example.com>
2018-01-22 15:14:12 +0100

    balik services, entity User, UserServiceJPA

--- src/main/java/forum/entity/User.java
+++ src/main/java/forum/entity/User.java
@@ -12,6 +12,33 @@ public class User {
 	private String login;
 	private String password;
 	private String email;
+	public int getIdent() {
+		return ident;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setIdent(int ident) {
+		this.ident = ident;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
 	
 	
 
